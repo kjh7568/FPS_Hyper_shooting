dcd7aea33101c1c7a53d124ad45c15c9c08a98e8
Gyeong816 <rwrwg159@gmail.com>
2025-06-09 11:42:14 +0900

    점프 완료

--- Assets/3.Script/PlayerController.cs
+++ Assets/3.Script/PlayerController.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
-    
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float jumpHeight = 2f;
+
     private CharacterController characterController;
+    private Vector3 velocity;
 
     private void Awake()
     {
@@ -17,12 +20,31 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         MovePlayer();
+        ApplyGravity();
     }
 
     private void MovePlayer()
     {
         Vector3 inputAxis = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-        
-        characterController.Move(inputAxis * (moveSpeed * Time.deltaTime));
+        Vector3 move = transform.TransformDirection(inputAxis);
+
+        characterController.Move(move * (moveSpeed * Time.deltaTime));
+
+        if (Input.GetKey(KeyCode.Space) && characterController.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(2f * jumpHeight * -gravity);
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        if (characterController.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        characterController.Move(velocity * Time.deltaTime);
     }
 }
