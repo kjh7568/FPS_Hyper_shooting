876d01e7008b2c5323de6a67f8f2c65ba3e34c88
Gyeong816 <rwrwg159@gmail.com>
2025-06-09 11:54:45 +0900

    마우스 회전 구현

--- Assets/3.Script/Player/PlayerController.cs
+++ Assets/3.Script/Player/PlayerController.cs
@@ -5,24 +5,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement Settings")]
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float gravity = -9.81f;
     [SerializeField] private float jumpHeight = 2f;
+    
+    [Header("Mouse Settings")]
+    [SerializeField] private float mouseSensitivity = 100f;
+    [SerializeField] private Transform playerCamera;
 
     private CharacterController characterController;
     private Vector3 velocity;
+    private float cameraPitch = 0f;
 
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
     }
 
+    private void Start()
+    {
+        SetCursor();
+    }
+
     private void Update()
     {
         MovePlayer();
         ApplyGravity();
+        RotatePlayer();
     }
 
+    private void SetCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    
     private void MovePlayer()
     {
         Vector3 inputAxis = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
@@ -46,5 +64,17 @@ public class PlayerController : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
 
         characterController.Move(velocity * Time.deltaTime);
+    }
+
+    private void RotatePlayer()
+    {
+        Vector2 mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * (mouseSensitivity * Time.deltaTime);
+        
+        transform.Rotate(Vector3.up * mouseInput.x);
+        
+        cameraPitch -= mouseInput.y;
+        cameraPitch = Mathf.Clamp(cameraPitch, -90f, 90f);
+
+        playerCamera.localEulerAngles = new Vector3(cameraPitch, 0f, 0f);
     }
 }
