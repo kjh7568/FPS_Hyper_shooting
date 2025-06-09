d626c47c9328b6b6eb4f87dd15c62b9f29c15dfd
Gyeong816 <rwrwg159@gmail.com>
2025-06-09 13:30:27 +0900

    좀비 프리셋 생성 및 스탯 구현

---
+++ Assets/3.Script/Zombie/ZombieStat.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ZombieStat
+{
+    public float health;
+    public float maxHealth;
+    public float damage;
+    public float moveSpeed;
+    public bool isCanRun;
+}
