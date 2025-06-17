ad3a3e1f1fe30167509db20a6812a9887aed0023
Gyeong816 <rwrwg159@gmail.com>
2025-06-17 16:30:53 +0900

    옵션 뜨도록 하고 UI 애니메이션도 함

--- Assets/3.Script/UI/MainUIManager.cs
+++ Assets/3.Script/UI/MainUIManager.cs
@@ -5,6 +5,7 @@ using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class MainUIManager : MonoBehaviour
 {
@@ -48,24 +49,83 @@ public class MainUIManager : MonoBehaviour
                 //todo 드롭 장비 종류에 따라 오버로딩 할 것
                 OpenTargetEquipmentPanel(target.GetComponent<DroppedItem>().dropedItem);
             }
+            else
+            {
+                isOpen = false;
+                targetPanel.SetActive(false);
+            }
         }
     }
     
     private void OpenTargetEquipmentPanel(Armor target)
     {
         if (isOpen) return;
-        
+
+        isOpen = true;
         targetPanel.SetActive(true);
-        StartCoroutine(OpenTargetPanelByAnimation_coroutine());
+        OpenTargetPanelByAnimation_coroutine(targetPanel.GetComponent<RectTransform>());
+
+        targetLevelText.text = $"LV. {target.currentStat.level}";
+        targetNameText.text = $"{target.data.armorName}";
+        targetTierText.text = $"{target.grade.ToString()}";
+        targetValueText.text = $"{target.currentStat.defense}";
+        SetDescription(target, targetDescriptionTexts);
+         
+        targetItemImage.sprite = target.data.icon;
     }
 
-    private IEnumerator OpenTargetPanelByAnimation_coroutine()
+    private void OpenTargetPanelByAnimation_coroutine(RectTransform targetRect)
     {
-        
+        targetRect.localScale = Vector3.one * 0.01f;
+
+        // 1초 동안 (EaseOut으로) 스케일 1까지 확대
+        targetRect.DOScale(Vector3.one, 0.5f).SetEase(Ease.OutBack);
     }
 
     private void OpenCurrentEquipmentPanel()
     {
         
+    }
+
+    private void SetDescription(Armor parts, TMP_Text[] descriptions)
+    {
+        int idx = 0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            descriptions[i].gameObject.SetActive(false);
+        }
+        
+        foreach (var option in parts.options)
+        {
+            descriptions[idx].gameObject.SetActive(true);
+
+            switch (option)
+            {
+                case SpecialEffect.DashCooldownReduction:
+                    descriptions[idx].text = "대시 쿨타임이 10% 감소합니다";
+                    break;
+                case SpecialEffect.MultiplierDefense:
+                    descriptions[idx].text = "방어력이 10% 증가합니다";
+                    break;
+                case SpecialEffect.IncreaseHealth:
+                    descriptions[idx].text = "최대 체력이 20 증가합니다";
+                    break;
+                case SpecialEffect.MultiplierHealth:
+                    descriptions[idx].text = "최대 체력이 10% 증가합니다";
+                    break;
+                case SpecialEffect.ReloadSpeedReduction:
+                    descriptions[idx].text = "재장전 속도가 10% 빨라집니다";
+                    break;
+                case SpecialEffect.MultiplierAttackDamage:
+                    descriptions[idx].text = "공격력이 5% 증가합니다";
+                    break;
+                case SpecialEffect.MultiplierMovementSpeed:
+                    descriptions[idx].text = "이동 속도가 10% 증가합니다";
+                    break;
+            }
+
+            idx++;
+        }
     }
 }
