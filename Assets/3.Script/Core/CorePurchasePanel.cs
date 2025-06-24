b4bb6a76fe968f6a48ae59a8873a3c53b8e86821
Yun0don <127651725+Yun0don@users.noreply.github.com>
2025-06-24 13:32:39 +0900

    코어 ui 작업완료 스펙 적용시작.

--- Assets/3.Script/Core/CorePurchasePanel.cs
+++ Assets/3.Script/Core/CorePurchasePanel.cs
@@ -1,37 +1,70 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
 
 public class CorePurchasePanel : MonoBehaviour
 {
     public static CorePurchasePanel Instance;
 
-    [SerializeField] private Button buyButton;
+    [System.Serializable]
+    public class PurchaseSlot
+    {
+        public Button button;
+        public TMP_Text costText;
+    }
+
+    [SerializeField] private List<PurchaseSlot> slots;
+
     private CoreDataSO selectedCore;
+    private int currentLevel = 0; // 현재 구매된 최고 레벨
 
     private void Awake()
     {
         Instance = this;
     }
 
-    public void SetSelectedCore(CoreDataSO data)
+    public void SetSelectedCore(CoreDataSO core)
     {
-        selectedCore = data;
+        selectedCore = core;
+        currentLevel = 0; // 혹은 PlayerData에서 불러오기
+
+        UpdateUI();
     }
 
-    private void Start()
+    private void UpdateUI()
     {
-        buyButton.onClick.AddListener(BuyCore);
-    }
-
-    private void BuyCore()
-    {
-        if (selectedCore == null) return;
-
-        var lv1 = selectedCore.levelStats.Find(x => x.level == 1);
-        if (lv1 != null)
+        for (int i = 0; i < slots.Count; i++)
         {
-            Debug.Log($"구매한 코어: {selectedCore.coreName} | 가격: {lv1.price}");
-            // 추후 PlayerData.Instance.UseGold(lv1.price); 등으로 연동
+            if (i < selectedCore.levelStats.Count)
+            {
+                var lvData = selectedCore.levelStats[i];
+                slots[i].costText.text = $"{lvData.price}Core";
+
+                int levelIndex = i; // 캡처 방지용
+                slots[i].button.onClick.RemoveAllListeners();
+                slots[i].button.onClick.AddListener(() => Buy(levelIndex));
+
+                // 순차 제한: 이전 레벨을 사야만 버튼 활성
+                slots[i].button.interactable = (i == currentLevel);
+            }
+            else
+            {
+                slots[i].button.gameObject.SetActive(false);
+                slots[i].costText.text = "";
+            }
         }
+    }
+
+    private void Buy(int levelIndex)
+    {
+        var data = selectedCore.levelStats[levelIndex];
+        Debug.Log($"[{selectedCore.coreName}] {data.level}레벨 구매! 가격: {data.price}G");
+
+        // 실제 구매 로직은 나중에 PlayerData.Instance.UseGold(data.price); 등으로 처리
+
+        currentLevel++; // 다음 레벨 구매 가능하게
+        UpdateUI();
+        CoreInfoPanel.Instance.DisplayCore(selectedCore); // Info 갱신
     }
 }
